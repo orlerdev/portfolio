0e678e5a0e328b2a576d489047cba46fa4d8ad87
Jared Orler <orlerdev@icloud.com>
2023-08-11 11:24:14 -0500

    createBrowserRouter started

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,48 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 import DefaultLayout from './layouts/DefaultLayout';
 import Home from './pages/Home';
 import About from './pages/About';
 import ProfessionalHistory from './pages/ProfessionalHistory';
 
-function App() {
-	return (
-		<Router>
-			<Switch>
-				<DefaultLayout>
-					<Route exact path='/' component={Home} />
-					<Route path='/about' component={About} />
-					<Route path='/professional-history' component={ProfessionalHistory} />
-				</DefaultLayout>
-			</Switch>
-		</Router>
-	);
-}
+createBrowserRouter([
+	{
+		path: '/',
+		element: <Home />
+	},
+	{
+		path: '/about',
+		element: <About />
+	},
+	{
+		path: '/professional-history',
+		element: <ProfessionalHistory />
+	}
+]);
+
+const App = () => {
+	return <DefaultLayout></DefaultLayout>;
+};
 
 export default App;
+
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import DefaultLayout from './layouts/DefaultLayout';
+// import Home from './pages/Home';
+// import About from './pages/About';
+// import ProfessionalHistory from './pages/ProfessionalHistory';
+
+// function App() {
+// 	return (
+// 		<Router>
+// 			<Switch>
+// 				<DefaultLayout>
+// 					<Route exact path='/' component={Home} />
+// 					<Route path='/about' component={About} />
+// 					<Route path='/professional-history' component={ProfessionalHistory} />
+// 				</DefaultLayout>
+// 			</Switch>
+// 		</Router>
+// 	);
+// }
+
+// export default App;
