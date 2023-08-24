4baed598eed720773c8a60ee77186bf660ce9738
Jared Orler <orlerdev@icloud.com>
2023-08-24 11:18:35 -0500

    about me page updates

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -25,8 +25,31 @@ const Entry = styled.div`
   width: 80vw;
 `;
 
+const TextWrapper = styled.div`
+  display:flex;
+  flex-direction:column;
+  row-gap:10px;
+`;
+
 const Title = styled.h2`
   flex: 1;
+`;
+
+const AboutBody = styled.ul`
+  max-width:70%;
+  list-style-type: none;
+  display:flex;
+  flex-direction:column;
+  row-gap:10px;
+  
+  li{
+    padding-left: 1em;
+    text-indent: -1em;
+  }
+  
+  li::before{
+    content: '- ';
+  }
 `;
 
 const Photo = styled.img`
@@ -47,19 +70,46 @@ const About = () => {
     <DefaultLayout>
       <Entries>
         <Entry>
+          <TextWrapper>
           <Title>&quot;2007-2012: USS Carl Vinson(CVN70)&quot;</Title>
+            <AboutBody>
+              <li>Proactively conducted 100 critical repairs and system overhauls resulting in a savings in excess of $30,000.00</li>
+              <li>Qualified Enlisted Surface Warfare Specialist</li>
+            </AboutBody>
+            </TextWrapper>
           <Photo src={photo1} alt="Entry photo" />
         </Entry>
         <Entry>
+          <TextWrapper>
           <Title>&quot;2012-2015: Recruit Training Command(RTC)&quot;</Title>
+            <AboutBody>
+              <li>Reviewed and corrected curriculum deficiencies, ensuring that the most up-to-date information was being provided to the Navy</li>
+              <li>Provided high-risk training to 78,000 Naval recruits, with zero safety mishaps, and a passing rate of 100%</li>
+              <li>Qualified as a Master Training Specialist</li>
+            </AboutBody>
+            </TextWrapper>
           <Photo src={photo2} alt="Entry photo" />
         </Entry>
         <Entry>
+          <TextWrapper>
           <Title>&quot;2015-2020: USS Michael Monsoor(DDG1001)&quot;</Title>
+            <AboutBody>
+              <li>Essential in the creation of unique ZUMWALT damage control doctrine</li>
+              <li>Delivered 300+ hours of dynamic damage control training and assessed 100+ casualty scenarios, resulting in a fully qualified crew</li>
+              <li>Served as Damage Control Warfare Manager, Damage Control Training Team Coordinator, Fire Marshal, Gas Free Engineer, Quality Assurance Officer, Education Services Officer, Command Managed Equal Opportunity Manager, and Motorcycle Safety Manager</li>
+            </AboutBody>
+            </TextWrapper>
           <Photo src={photo3} alt="Entry photo" />
         </Entry>
         <Entry>
+          <TextWrapper>
           <Title>&quot;2020-2022: HSM-41 Seahawks&quot;</Title>
+            <AboutBody>
+              <li>Responsible for supervising 120 personnel in the maintenance and upkeep of all squadron working spaces, establishing a structured and safe environment</li>
+              <li>Personnel onboarding manager and Leading Petty Officer. Implemented a comprehensive overhaul of unreliable standards and procedures, leading to an 80% reduction in administrative delinquencies.</li>
+              <li>Conducted 100+ personnel performance evaluations, facilitating professional development of the crew.</li>
+            </AboutBody>
+            </TextWrapper>
           <Photo src={photo4} alt="Entry photo" />
         </Entry>
       </Entries>
