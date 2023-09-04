a0c84bbcae326408d54d325147b92a3bf11c99d2
Jared Orler <orlerdev@icloud.com>
2023-09-04 18:28:36 -0500

    media query tweaks

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -3,35 +3,37 @@ import { media } from '../styles/utils.js';
 import headshot from '../assets/Headshot.png';
 
 const AvatarWrapper = styled.div`
-  position: relative;
-  width: auto;
-  height: 45vh;
-  border-radius: 1.2rem;
-  background: ${props => props.theme.colors.frosted};
-  background-image: ${props => props.theme.colors.backgroundImage};
-  background-blend-mode: overlay;
-  box-shadow: ${props => props.theme.effects.boxShadow};
-  overflow: hidden;
+	position: relative;
+	width: auto;
+	height: 45vh;
+	border-radius: 1.2rem;
+	background: ${(props) => props.theme.colors.frosted};
+	background-image: ${(props) => props.theme.colors.backgroundImage};
+	background-blend-mode: overlay;
+	box-shadow: ${(props) => props.theme.effects.boxShadow};
+	overflow: hidden;
 
-  ${media.small`
+	${media.small`
 		max-width:100%;
 		max-height:100%;
+    height:100%;
 	`}
 `;
 
 const AvatarImage = styled.img`
-  height: 100%;
-  width: 100%;
-  aspect-ratio: auto;
+	max-height: 100%;
+	height: 100%;
+	width: 100%;
+	aspect-ratio: auto;
 `;
 
 const Avatar = () => {
-  return (
-    <>
-      <AvatarWrapper>
-        <AvatarImage src={headshot} alt="Headshot" />
-      </AvatarWrapper>
-    </>
-  );
+	return (
+		<>
+			<AvatarWrapper>
+				<AvatarImage src={headshot} alt='Headshot' />
+			</AvatarWrapper>
+		</>
+	);
 };
 export default Avatar;
