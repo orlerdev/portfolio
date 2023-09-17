82af00449b0ebae3a01672b6884d30d0223f0f90
Jared Orler <orlerdev@icloud.com>
2023-09-17 14:03:32 -0500

    projects description added, projects page data population improvments

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,39 +1,101 @@
+import {useEffect, useState} from 'react';
 import styled from '@emotion/styled';
 import DefaultLayout from '../layouts/DefaultLayout.jsx';
-import DNDB from '../assets/DNDB.png';
+import {projects} from "../data/projects.js";
 
 
 const ProjectsWrapper = styled.div`
-  display:flex;
-  flex-direction:column;
-  justify-content: flex-start;
-  height:100vh;
-  row-gap:10px;
-  padding:10px;
-  margin-top:50px;
-  background: ${props => props.theme.colors.frosted};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  row-gap: 10px;
+  padding: 10px;
+  margin-top: 50px;
   color: ${props => props.theme.colors.altText};
-  box-shadow: ${props => props.theme.effects.darkBoxShadow};
-  border-radius: 1em;
 `;
+
+const ProjectContainer = styled.div`
+  position: relative;
+`;
+
 const ProjectTitle = styled.h2`
-  align-self: center;
   font-size: 3em;
+  border-bottom: 5px solid ${props => props.theme.colors.altText};
+  width: 100%;
+  text-align: center;
 `;
+
+const ProjectLink = styled.a`
+  margin-top: 1rem;
+`;
+
+const PhotoWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
 const ProjectPhoto = styled.img`
-  width:100%;
-  height:auto;
+  width: 80%;
+  height: auto;
   border-radius: 1em;
 `;
 
+const ProjectDescription = styled.p`
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  transform:translateX(-50%);
+  width: 80%;
+  height: fit-content;
+  padding:1rem;
+  border-radius: 0 0 1rem 1rem;
+  background: ${props => props.theme.colors.frosted};
+  backdrop-filter: blur(20px);
+  pointer-events: none;
+  color: ${props => props.theme.colors.altText};
+  transition: all ease .3s;
+  z-index: 3;
+`;
+
+
 const Projects = () => {
+  const [hoveredProject, setHoveredProject] = useState(null);
+
+
   return (
     <DefaultLayout>
       <ProjectsWrapper>
-          <ProjectTitle>Dungeons & Dust Bunnies</ProjectTitle>
-          <a href='https://dndb.me' target='_blank' rel="noreferrer"><ProjectPhoto src={DNDB} alt='Dungeons and Dust Bunnies home page photo' /></a>
+        {projects.map(project => (
+          <ProjectContainer key={project.title}
+                            onMouseEnter={() => setHoveredProject(project)}
+                            onMouseLeave={() => setHoveredProject(null)}
+          >
+            <ProjectTitle>{project.title}</ProjectTitle>
+            <ProjectLink href={project.link} target='_blank' rel='noreferrer'>
+              <PhotoWrapper>
+                <ProjectPhoto src={project.image} alt='Project Photo'/>
+              </PhotoWrapper>
+            </ProjectLink>
+            {hoveredProject &&
+              <ProjectDescription>{project.body}</ProjectDescription>
+            }
+          </ProjectContainer>
+        ))}
       </ProjectsWrapper>
     </DefaultLayout>
   );
 };
 export default Projects;
+
+
+// <ProjectTitle>Dungeons & Dust Bunnies</ProjectTitle>
+// <ProjectLink href='https://dndb.me' target='_blank' rel="noreferrer">
+//   <PhotoWrapper>
+//     <ProjectPhoto src={DNDB} alt='Dungeons and Dust Bunnies home page photo'/>
+//   </PhotoWrapper>
+// </ProjectLink>
